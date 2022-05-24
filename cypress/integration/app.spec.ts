4f5974428e0858e523307da52bd2a8a978163530
Endika Orube <endika.orube@biko2.com>
2022-05-24 12:24:02 +0200

    test: añade test e2e de validación de navegación

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -1,18 +1,41 @@
 export {};
 
 describe("Navigation", () => {
-  it("should navigate to the static page", () => {
-    // Start from the index page
+  it("should navigate from home to pokemon details and back to home", () => {
+    const pokemon = {
+      id: 1,
+      name: "bulbasaur",
+    };
+
     cy.visit("http://localhost:3000/");
 
-    // Find a link with an href attribute containing "about" and click it
-    cy.get('a[href*="render/static"]').click();
+    cy.intercept({
+      method: "GET",
+      url: `https://pokeapi.co/api/v2/pokemon/${pokemon.id}`,
+    }).as("getAllPokemonsFirstPokemon");
 
-    // The new url should include "/about"
-    cy.url().should("includes", "/render/static");
+    cy.wait("@getAllPokemonsFirstPokemon")
+      .its("response.statusCode")
+      .should("equal", 200);
 
-    // The new page should contain an h1 with "About page"
-    cy.get("h1").contains("STATIC").click();
+    cy.get(`a[href*="details/${pokemon.name}"]`).click();
+
+    cy.url().should("includes", `/details/${pokemon.name}`);
+
+    cy.get("main").should("have.text", "Cargando...");
+
+    cy.intercept({
+      method: "GET",
+      url: `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`,
+    }).as("getPokemonDetails");
+
+    cy.wait("@getPokemonDetails")
+      .its("response.statusCode")
+      .should("equal", 200);
+
+    cy.get(`[alt="${pokemon.id}"]`).should("be.visible");
+
+    cy.get('a[href*="/"]').click();
 
     cy.url().should("includes", "/");
   });
